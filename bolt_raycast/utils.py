24a83911e0f524a707bd6426663b33332b61ef5d
Stefan VDV <vdvman88@gmail.com>
2022-12-12 20:15:16 +1100

    feat: Add helper for nested "and" predicates

--- bolt_raycast/utils.py
+++ bolt_raycast/utils.py
@@ -4,3 +4,15 @@ from nbtlib import serialize_tag
 
 def to_snbt(node: AstNbt) -> str:
     return serialize_tag(node.evaluate(), compact=True, quote="'")
+
+
+def nested_and_predicate(*args: dict) -> dict:
+    return {
+        "condition": "minecraft:inverted",
+        "term": {
+            "condition": "minecraft:alternative",
+            "terms": [
+                {"condition": "minecraft:inverted", "term": term} for term in args
+            ],
+        },
+    }
